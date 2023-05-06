22a2c9204086444bb16c7ac020ceefb7ff667c5f
legnwoo <leekw071016@gmail.com>
2023-05-06 17:50:37 +0900

    리터럴

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ var result =x + 3;
 var jabascript = 10;//변수 javascript와 JavaScript는 서로 다른 두 개의 변수로 인식됨.
 var JabaScript = 20;
 var Script = 30;//변수의 선언은 var 키워드로만 할 수 있으면 Var는 동작하지 않음.
+
+12//숫자 리터럴
+"JavaScript"//문자 리터럴
+"안녕하세요"//문자 리터럴
+true//불리언 리터럴
