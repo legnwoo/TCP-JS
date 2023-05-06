772127f915ca2acea2317df126bd806786c437e5
legnwoo <leekw071016@gmail.com>
2023-05-06 17:42:37 +0900

    자바스크립트 문법

--- script.js
+++ script.js
@@ -1,25 +1,2 @@
-var x = 10;
-var result = x + 3;
-
-var javascript = 10; // 변수 javascript와 JavaScript는 서로 다른 두 개의 변수로 인식됨.
-var JavaScript = 20;
-var Script = 30; // 변수의 선언은 var 키워드로만 할 수 있으면 Var는 동작하지 않음.
-
-12            // 숫자 리터럴
-"JavaScript"  
-"안녕하세요"
-true         
-
-var firstVar = 10;// Camel Case 방식
-function my_first_func() { // Underscore Case 방식
-    var firstLocalVar = 20;  // Camel Case 방식
-}
-
-var firstVar = 10;      // var는 변수의 정의를 위해 예약된 키워드입니다.
-
-function myFirstFunc() {  // function은 함수의 정의를 위해 예약된 키워드입니다.
-
-    var secondVar = 20; // var는 변수의 정의를 위해 예약된 키워드입니다.
-
-}
-
+var x =10;
+var result =x + 3;
