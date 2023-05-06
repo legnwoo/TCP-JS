58d198ca675a1d4b2cfeef51628dd5648febd85f
legnwoo <leekw071016@gmail.com>
2023-05-06 22:24:31 +0900

    document.write() 메소드

--- script.js
+++ script.js
@@ -23,8 +23,6 @@ var secondVar = 20; // var는 변수의 정의를 위해 예약된 키워드입�
 1.한 줄 주석://주석문
 2.여러줄 주석:/*주석문*/
 
-var x = 10;
-
 // var y = 20; 한 줄의 주석문 : 이 부분은 실행되지 않을 것입니다.
 /*
 x = x + y;
@@ -37,11 +35,21 @@ document.getElementById("text").innerHTML = x;
     // 이렇게 두 줄 주석 안에 또 다른 한 줄 주석을 삽입할 수 있습니다.
 주석입니다. */
 
-<Script>
-    function alertDialogBox(){
-        alert("확인을 누를 때까지 다른 작업을 수행할 수 없어요!")
-    }
-</Script>
+/* 여러 줄
+// 이렇게 두 줄 주석 안에 또 다른 한 줄 주석을 삽입할 수 있습니다.
+주석입니다. */
+    
+    <script>
+        function alertDialogBox(){alert("확인을 누를 때까지 다른 작업을 수행할 수 없어요!")}
+        
+        var str = document.getElementById("text");
+        str.innerHTML = "이 문장으로 바뀌었습니다!";
+
+        document.write(4*5)
+    </script>
+
+   
+
 
 
 
